3d07d79aa5761c86219e78ab8c803657460335de
Daniel Arce <8534425+armad@users.noreply.github.com>
2024-02-11 00:20:19 -0600

    subiendo fig02_01

---
+++ cursos/00831/ejercicios/ejemplos del libro/fig02_01.cpp
@@ -0,0 +1,9 @@
+// Programa de impresión de texto.
+#include <iostream> // permite que el programa envie datos a la pantalla
+
+// la función main comienza la ejecución del programa
+int main() {
+    std::cout << "Bienvenido a C++"; // mostrar mensaje
+
+    return 0; // indica que el programa terminó exitosamente
+} // fin de la función principal
